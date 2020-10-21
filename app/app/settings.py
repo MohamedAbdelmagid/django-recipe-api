ee731cb195dd4addd7d1aef1a1b27e7e9f98532c
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-10-21 10:30:16 -0500

    Setup docker & django for files uploads

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,5 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = '/volume/web/static'
+MEDIA_ROOT = '/volume/web/media'
+
 
 AUTH_USER_MODEL = 'core.User'
